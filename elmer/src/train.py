bc3158c4871a1971ca84f8018e5dc4dc5aad8157
rutheferd <aaustinruth@gmail.com>
2022-06-01 23:47:36 -0400

    Day 3 15 minutes, finishing up dataset function

--- elmer/src/train.py
+++ elmer/src/train.py
@@ -15,6 +15,7 @@ logging.basicConfig(
 
 AUDIO_SUBFOLDER = "audio"
 NOISE_SUBFOLDER = "noise"
+SHUFFLE_SEED = 43
 SCALE = 0.5
 
 def paths_and_labels_to_dataset(audio_paths, labels):
@@ -49,7 +50,78 @@ def audio_to_fft(audio):
     return tf.math.abs(fft[:, : (audio.shape[1] // 2), :])
 
 
-def build_dataset():
+def build_dataset(data_audio_path, val_split, batch_size):
+    class_names = os.listdir(data_audio_path)
+    print(
+        "Our class names: {}".format(
+            class_names,
+        )
+    )
+
+    audio_paths = []
+    labels = []
+    for label, name in enumerate(class_names):
+        print(
+            "Processing speaker {}".format(
+                name,
+            )
+        )
+        dir_path = Path(data_audio_path) / name
+        speaker_sample_paths = [
+            os.path.join(dir_path, filepath)
+            for filepath in os.listdir(dir_path)
+            if filepath.endswith(".wav")
+        ]
+        audio_paths += speaker_sample_paths
+        labels += [label] * len(speaker_sample_paths)
+
+    print(
+        "Found {} files belonging to {} classes.".format(len(audio_paths), len(class_names))
+    )
+
+    # Shuffle
+    rng = np.random.RandomState(SHUFFLE_SEED)
+    rng.shuffle(audio_paths)
+    rng = np.random.RandomState(SHUFFLE_SEED)
+    rng.shuffle(labels)
+
+    # Split into training and validation
+    num_val_samples = int(val_split * len(audio_paths))
+    print("Using {} files for training.".format(len(audio_paths) - num_val_samples))
+    train_audio_paths = audio_paths[:-num_val_samples]
+    train_labels = labels[:-num_val_samples]
+
+    print("Using {} files for validation.".format(num_val_samples))
+    valid_audio_paths = audio_paths[-num_val_samples:]
+    valid_labels = labels[-num_val_samples:]
+
+    # Create 2 datasets, one for training and the other for validation
+    train_ds = paths_and_labels_to_dataset(train_audio_paths, train_labels)
+    train_ds = train_ds.shuffle(buffer_size=batch_size * 8, seed=SHUFFLE_SEED).batch(
+        batch_size
+    )
+
+    valid_ds = paths_and_labels_to_dataset(valid_audio_paths, valid_labels)
+    valid_ds = valid_ds.shuffle(buffer_size=32 * 8, seed=SHUFFLE_SEED).batch(32)
+
+
+    # Add noise to the training set
+    # train_ds = train_ds.map(
+    #     lambda x, y: (add_noise(x, noises, scale=SCALE), y),
+    #     num_parallel_calls=tf.data.AUTOTUNE,
+    # )
+
+    # Transform audio wave to the frequency domain using `audio_to_fft`
+    train_ds = train_ds.map(
+        lambda x, y: (audio_to_fft(x), y), num_parallel_calls=tf.data.AUTOTUNE
+    )
+    train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+
+    valid_ds = valid_ds.map(
+        lambda x, y: (audio_to_fft(x), y), num_parallel_calls=tf.data.AUTOTUNE
+    )
+    valid_ds = valid_ds.prefetch(tf.data.AUTOTUNE)
+
     pass
 
 def train(data_path, val_split, sampling_rate, batch_size, num_epochs):
@@ -58,4 +130,6 @@ def train(data_path, val_split, sampling_rate, batch_size, num_epochs):
 
 def run(data_path, val_split, sampling_rate, batch_size, num_epochs):
     sentence = train(data_path, val_split, sampling_rate, batch_size, num_epochs)
+    data_audio_path = os.path.join(data_path, AUDIO_SUBFOLDER)
+    data_noise_path = os.path.join(data_path, NOISE_SUBFOLDER)
     print(sentence)
